d837400fce5f510b1350c96c9a3cbe074d4a6430
khanqasim123 <qasim2905@gmail.com>
2025-10-29 16:30:02 +0500

    adding Benefits

--- javascript.js
+++ javascript.js
@@ -8,13 +8,13 @@ async function loadProducts() {
     productGrid.innerHTML = products
       .map(
         (product) => `
-        <div class="shadow-md rounded-2xl bg-blue-100 ">
-        <div class="text-center p-5">
-        <img src=${product.image} class="w-[200px] h-[180px] mx-auto" alt="no picture">
-        <h6 class="font-semibold text-lg mb-2 truncate">${product.title}</h6>
-        <p>$ ${product.price}</p>
-        <button class= "bg-blue-600 text-white font-semibold text-sm px-4 py-2 hover:shadow-md transition w-fit md:ml-4 rounded-2xl">Add to card</button>
-        </div>
+        <div class="shadow hover:shadow-md rounded-2xl bg-blue-100 ">
+          <div class="text-center p-5">
+            <img src=${product.image} class="w-[200px] h-[180px] mx-auto" alt="no picture">
+            <h6 class="font-semibold text-lg mb-2 truncate">${product.title}</h6>
+            <p>$ ${product.price}</p>
+            <button class= "bg-blue-600 text-white font-semibold text-sm px-4 py-2 hover:shadow-md transition w-fit md:ml-4 rounded-2xl">Add to card</button>
+          </div>
         </div>
       `
       )
